365fec78a4c5f0e0e5ef754c71f65f479f11d407
Francisco <belcboo@gmail.com>
2018-09-19 09:17:14 -0400

    Models + server almost completed.

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ var app = express();
 
 //MIDDLEWARE
 app.use(logger("dev"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 // Connect to the Mongo DB
@@ -32,12 +34,32 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-app.get("/scrape", function(req, res){
+app.get("/scrape", function(req, res) {
 
-  //We request the body using axios.
-  axios.get("http://www.nytimes.com").then(function(response){
+      //We request the body using axios.
+      axios.get("http://www.nytimes.com").then(function(response) {
 
-    //Loading response into cherio.
-    var $ = cherio.load(response.data);
-  })
-})
+        //Loading response into cherio.
+        var $ = cherio.load(response.data);
+
+        // Looking for h2 tags.
+        $("article h2").each(function(i, element) {
+          // Save an empty result object
+          var result = {};
+
+          // Add the text and href of every link, and save them as properties of the result object
+          result.title = $(this)
+            .children("a")
+            .text();
+          result.link = $(this)
+            .children("a")
+            .attr("href");
+        })
+      })
+      //ROUTES
+
+
+      //STARTING EXPRESS SERVER
+      app.listen(PORT, function() {
+        console.log("App running on port " + PORT + "!");
+      });
